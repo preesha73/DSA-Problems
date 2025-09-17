02a0db6834bcf37e291921fed561cc4cd8dddaf5
Preesha Vashisth <ptpk2810@gmail.com>
2025-09-17 15:12:46 +0530

    Time: 2 ms (96.79%), Space: 42 MB (27.54%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stk = new Stack<>();
+        for(char ch: s.toCharArray()){
+            if(ch == '('){
+                stk.push(')');
+            } else if(ch == '{'){
+                stk.push('}');
+            } else if(ch == '['){
+                stk.push(']');
+            } else {
+                if(stk.isEmpty()){
+                    return false;
+                } else if(stk.pop() != ch){
+                    return false;
+                }
+            }
+        }
+        return stk.isEmpty();
+    }
+}
