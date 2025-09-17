2e1fe11f189dcb6ae915e004848e9d13b7312042
Preesha Vashisth <ptpk2810@gmail.com>
2025-09-17 16:29:47 +0530

    Time: 7 ms (15.42%), Space: 63.3 MB (50.93%) - LeetHub

---
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minDepth(TreeNode root) {
+        // Base case...
+        // If the subtree is empty i.e. root is NULL, return depth as 0...
+        if(root == null)  return 0;
+        // Initialize the depth of two subtrees...
+        int leftDepth = minDepth(root.left);
+        int rightDepth = minDepth(root.right);
+        // If the both subtrees are empty...
+        if(root.left == null && root.right == null)
+            return 1;
+        // If the left subtree is empty, return the depth of right subtree after adding 1 to it...
+        if(root.left == null)
+            return 1 + rightDepth;
+        // If the right subtree is empty, return the depth of left subtree after adding 1 to it...
+        if(root.right == null)
+            return 1 + leftDepth;
+        return Math.min(leftDepth, rightDepth) + 1;
+    }
+}
